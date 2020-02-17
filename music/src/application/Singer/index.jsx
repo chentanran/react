cac1317339b9bb5b69b1599d1a80c9a1a7093087
tanran <215920697@qq.com>
2020-02-17 09:21:01 +0800

    音乐播放页面开发

--- music/src/application/Singer/index.jsx
+++ music/src/application/Singer/index.jsx
@@ -4,32 +4,50 @@ import { CSSTransition } from 'react-transition-group'
 import Header from '../../baseUI/header'
 import Scroll from '../../components/scroll'
 import SongsList from '../../components/SongList'
+import { HEADER_HEIGHT } from '../../api/config'
+import { useSelector, useDispatch } from 'react-redux'
+import { changeEnterLoading, getSingerInfo } from './store/actionCreator'
+import Loading from '../../baseUI/loading/index'
 
 function Singer(props) {
   const [showStatus, setShowStatus] = useState(true)
 
+  const artistImmu = useSelector(state => state.getIn(['singer', 'artist']))
+  const songsImmu = useSelector(state => state.getIn(['singer', 'songsOfArtist']))
+  const loading = useSelector(state => state.getIn(['singer', 'loading']))
+
+  const artist = artistImmu ? artistImmu.toJS() : {}
+  const songs = songsImmu ? songsImmu.toJS() : []
+
+  const dispatch = useDispatch()
+  const id = props.match.params.id
+  useEffect(() => {
+    dispatch(changeEnterLoading(true))
+    dispatch(getSingerInfo(id))
+  }, [dispatch, id])
+
   // mock
-  const artist = {
-    picUrl: "https://p2.music.126.net/W__FCWFiyq0JdPtuLJoZVQ==/109951163765026271.jpg",
-    name: "薛之谦",
-    hotSongs: [
-      {
-        name: "我好像在哪见过你",
-        ar: [{ name: "薛之谦" }],
-        al: {
-          name: "薛之谦专辑"
-        }
-      },
-      {
-        name: "我好像在哪见过你",
-        ar: [{ name: "薛之谦" }],
-        al: {
-          name: "薛之谦专辑"
-        }
-      },
-      // 省略 20 条
-    ]
-  }
+  // const artist = {
+  //   picUrl: "https://p2.music.126.net/W__FCWFiyq0JdPtuLJoZVQ==/109951163765026271.jpg",
+  //   name: "薛之谦",
+  //   hotSongs: [
+  //     {
+  //       name: "我好像在哪见过你",
+  //       ar: [{ name: "薛之谦" }],
+  //       al: {
+  //         name: "薛之谦专辑"
+  //       }
+  //     },
+  //     {
+  //       name: "我好像在哪见过你",
+  //       ar: [{ name: "薛之谦" }],
+  //       al: {
+  //         name: "薛之谦专辑"
+  //       }
+  //     },
+  //     // 省略 20 条
+  //   ]
+  // }
 
   const songScroll = useRef()
   const collectButton = useRef()
@@ -44,17 +62,55 @@ function Singer(props) {
 
   useEffect(() => {
     let h = imageWrapper.current.offsetHeight
-    console.log(imageWrapper.current)
     songScrollWrapper.current.style.top = `${h-OFFSET}px`
     initialHeight.current = h
     // 遮罩放下面, 裹住歌曲列表
     layer.current.style.top = `${h-OFFSET}px`
     songScroll.current.refresh()
   }, [])
-
+  //
   const setShowStatusFalse = useCallback (() => {
     setShowStatus (false);
   }, [])
+
+  const handleScroll = useCallback((pos) => {
+    const height = initialHeight.current
+    const newY = pos.y
+    const imageDOM = imageWrapper.current
+    const buttonDOM = collectButton.current
+    const headerDOM = header.current
+    const layerDOM = layer.current
+    const minScrollY = -(height-OFFSET) + HEADER_HEIGHT
+    // 滑动距离占图片高度的百分比
+    const percent = Math.abs(newY / height)
+    // 往下拉
+    // console.log(newY, minScrollY, percent)
+    if (newY > 0) {
+      imageDOM.style['transform'] = `scale(${1 + percent})`
+      buttonDOM.style['transform'] = `translate3d(0, ${newY}px, 0)`
+      layerDOM.style.top = `${height - OFFSET + newY}px`
+    } else if (newY >= minScrollY) { // 往上滑动
+      layerDOM.style.top = `${height - OFFSET - Math.abs(newY)}px`
+      // 提高层级
+      layerDOM.style.zIndex = 1
+      imageDOM.style.paddingTop = '75%'
+      imageDOM.style.height = 0
+      imageDOM.style.zIndex = -1
+      // 按钮跟着移动且渐渐变透明
+      buttonDOM.style['transform'] = `translate3d(0, ${newY}px, 0)`
+      buttonDOM.style['opacity'] = `${1 - percent * 2}`
+    } else if (newY < minScrollY) {
+      // 往上滑动，超过header部分
+      layerDOM.style.top = `${HEADER_HEIGHT - OFFSET}px`
+      layerDOM.style.zIndex = 1
+      // 防止溢出的歌单内容遮住 Header
+      headerDOM.style.zIndex = 100
+      // 图片高度与Header一致
+      imageDOM.style.height = `${HEADER_HEIGHT}px`
+      imageDOM.style.paddingTop = 0
+      imageDOM.style.zIndex = 99
+    }
+  },[])
 
   return (
     <CSSTransition
@@ -76,13 +132,16 @@ function Singer(props) {
         </CollectButton>
         <BgLayer ref={layer}></BgLayer>
         <SongListWrapper ref={songScrollWrapper}>
-          <Scroll ref={songScroll}>
+          <Scroll ref={songScroll} onScroll={handleScroll}>
             <SongsList
-              songs={artist.hotSongs}
+              songs={songs}
               showCollect={false}
             ></SongsList>
           </Scroll>
         </SongListWrapper>
+        {
+          loading ? <Loading></Loading> : null
+        }
       </Container>
     </CSSTransition>
   )
--- music/src/baseUI/header/index.jsx
+++ music/src/baseUI/header/index.jsx
@@ -32,6 +32,7 @@ const Header = React.forwardRef((props, ref) => {
     <HeaderContainer ref={ref}>
       <i className="iconfont back" onClick={handleClick}>&#xe655;</i>
       {
+        // eslint-disable-next-line
         isMarquee ? <marquee><h1>{title}</h1></marquee> : <h1>{title}</h1>
       }
     </HeaderContainer>
