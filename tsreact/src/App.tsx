e238fac3cc46645dadf314666a756cf4d2c0d042
tanran <215920697@qq.com>
2020-08-19 23:39:50 +0800

    context

--- tsreact/src/App.tsx
+++ tsreact/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Hello from './component/Helloword'
 import Button, { ButtonSize, ButtonType } from './component/Button/button'
+import Menu from './component/Menu/Menu'
+import MenuItem from './component/Menu/MenuItem'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu>
+          <MenuItem index={1}>娃哈哈</MenuItem>
+          <MenuItem index={2}>爽歪歪</MenuItem>
+          <MenuItem index={3}>看看看</MenuItem>
+          <MenuItem index={4}>听听听</MenuItem>
+        </Menu>
         <Button
           size={ButtonSize.Large}
           btnType={ButtonType.Link}
